5d33a57e07168c8f6ae646c20fd3a7e87b37925d
Kyle Kaplan <kfkaplan@grad11kk.as.utexas.edu>
2015-12-14 16:15:09 -0600

    Fixed issue with position angle for building datacubes not giving the correct PA if < 90 degrees.  If you are making a datacube please update to this version of makedatacube.py

--- make_datacube.py
+++ make_datacube.py
@@ -1,4 +1,4 @@
-#Library for making and processing the IGRINS datacube of the Orion Bar
+#Library for making and processing the IGRINS datacube
 from pylab import *
 from bottleneck import *
 from scipy.ndimage import zoom #For resizing images
@@ -9,26 +9,32 @@ from plotspec import * #Import plotspec library
 import h2 #Import H2 library
 from numpy import round
 
-
-
-#~~~~~~~~~~~~~~~~~~~~SCIENCE TARGET INFORMATION~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#~~~~~~~~~~~~~~~MODIFY THESE PARAMETERS FOR IGRINS DATA~~~~~~~~~~~~~~~~
 n_pointings = 6 #Number of slit positions used in scan
-x_expand = 4 #Number of times to expand along the x axis
-save.name('Orion Bar Cube') #Name to save results as
-date = 20141023 #Date (directory name) of observations
-stdno = 126 #Standard star frame no.
-waveno = 118 #Sky frame no. for using OH lines to corrected wave solution in PLP
-#skyno = 130 #Old alternate wavelength solution from telluric absoprtion lines in std star, from Kevin's code 
-spectral_lines = lines('H2_orion_bar.dat', delta_v = 8.0) #Spectral lines to use (selected for the Orion Bar)
-v_shift_file = 'line_corrections/orion_bar_00.dat' #File storing how to shift lines in velocity space to compensate for a bad wavelength solution
-B = 6.412 #B and V magnitudes of the std star HD 34317
-V = 6.410
-framenos = [123,119,120,121,125,133] #Frame numbers for pointings for map going from SW -> NE
-xshift = array([0.0, 1.0, 2.0, 3.0, 4.0, 5.0]) #Placement of slit in arcsecond space (1 arsec = 4 pixels) perpendicular to the slit
+save.name('Datacube Name') #Name to save results as
+date = 20141125 #Date (directory name) of observations
+stdno = 110 #Standard star frame no.
+waveno = 120 #Sky frame no. for using OH lines to corrected wave solution in PLP
+spectral_lines = lines('H2_datacube.dat', delta_v = 8.0) #Spectral lines to use
+B = 7.412 #B and V magnitudes of the std star HD 34317
+V = 7.410
+a0v_HI_y_scale = 1.0 #Scale HI line depth for A0V star
+a0v_HI_gauss_smooth = 1.0 #Gaussian smooth HI lines for A0V star ()
+framenos = [124,120,121,122,126,134] #Frame numbers for pointings for map going from SW -> NE
+exposure = [1.0, 1.0, 1.0, 1.0, 1.0, 1.0] #Relative exposure times for each frame, default = 1.0 but can be set to another value to equalize across whole datacube
+xshift = array([0.0, 1.0, 2.0, 3.0, 4.0, 5.0]) #Placement of slit in arcsecond space (1 arsec = 4 pixels) perpendicular to the slit (typically 1'' apart)
 yshift = array([0.0, 0.0, 0.0, 0.0 ,0.0 ,0.0]) #Placement of slit in arcsecond space (1 arsec = 4 pixels) parallel to the slit
+#~~~~~~~~~~~~~~~MODIFY THESE PARAMETERS FOR ASTROMETRY~~~~~~~~~~~~~~~~
+ra = 83.8319	#RA of reference pixel in decimal degrees
+dec = -5.4245	#Dec of reference pixel in decimal degrees
+x_reference_pixel = 3.0 #Set reference pixel perpendicular to the slit (x-axis), NOTE: these are pixel coordinates NOT ARCSECONDS!
+y_reference_pixel = 30.0 #Set reference pixel along slit length (y-axis)
+pa = 135.0 #Position Angle of slit on the sky (East of north) in degrees
+#~~~~~~~~~~~~~ONLY MODIFY THESE PARAMETERS IF YOU KNOW WHAT YOU ARE DOING~~~~~~~~~~~~~~~~
+plate_scale = 0.25 #Plate scale in arcseconds per pixel
+x_expand = 1.0/plate_scale #Number of times to expand along the x axis perpendicular to the slit, this  make the x and y plat
 #Shape of final 4D array in the form of (# of spectral lines, pixels along slit,velocity, pixels prependicular to  slit, ), x_expand expands along the x-axis (pointings) to make place scale same in x & y
 cube_size = [len(spectral_lines.label), slit_length + 5, 200, n_pointings*x_expand + 4] 
-plate_scale = 0.25 #Plate scale in arcseconds per pixel
 
 
 #~~~~~~~~~~~~~~~~~~~~FITS HEADER INFORMATION~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -36,33 +42,33 @@ def save(line_cube, fname): #Save datacube for an extracted cube for a spectral 
 	n_dimensions = ndim(line_cube) #Calculate number of dimensions
 	cube_fits = fits.PrimaryHDU(line_cube) #Open astropy fits object
 	cube_fits.header['RADECSYS'] = 'FK5' #Define coord system
-	cube_fits.header['EQUINOX'] = 2000 #Define coord system
+	cube_fits.header['EQUINOX'] = 2000 #Define equinox, here set to J2000
 	cube_fits.header['CTYPE1'] = 'RA--TAN' #Set unit to declination
 	cube_fits.header['CTYPE2'] = 'DEC--TAN' #Set unit for slit length to something generic
 	if n_dimensions == 3: #If velocity information exists.... else fits file saved will be a 2D image
 		cube_fits.header['CTYPE3'] = 'km/s' #Set unit to "Optical velocity" (I know it's really NIR but whatever...)
-	cube_fits.header['CRVAL1'] = 83.8319	 #RA of reference pixel
-	cube_fits.header['CRVAL2'] = -5.4245 #Dec of reference pixel
+	cube_fits.header['CRVAL1'] = ra	 #RA of reference pixel
+	cube_fits.header['CRVAL2'] = dec #Dec of reference pixel
 	if n_dimensions == 3: #If velocity information exists.... else fits file saved will be a 2D image
 		cube_fits.header['CRVAL3'] = 0.0 #Velocity of reference pixel
-	cube_fits.header['CRPIX1'] = 3.0 #Set zero point along the x-axis (pointings)
-	cube_fits.header['CRPIX2'] = 30.0 #Set zero point to 0 pixel for slit length
+	cube_fits.header['CRPIX1'] = x_reference_pixel #Set zero point along the x-axis (pointings)
+	cube_fits.header['CRPIX2'] = y_reference_pixel #Set zero point to 0 pixel for slit length
 	if n_dimensions == 3: #If velocity information exists.... else fits file saved will be a 2D image
 		cube_fits.header['CRPIX3'] = (velocity_range / velocity_res) + 1 #Set zero point to where v=0 km/s (middle of stamp)
 		cube_fits.header['CDELT3'] = velocity_res #Set zero point to where v=0 km/s (middle of stamp)
-	plate_scale = 0.25*(1./3600.) #Set plate scale to 0.25"
-	angle = 45.0 * pi / 180.0 #Rotation angle converted from degrees into radians
+	fits_plate_scale = plate_scale*(1./3600.) #Set plate scale to 0.25"
+	angle =  (pa + 270.0) * pi / 180.0 #Rotation angle converted from degrees into radians
 	x_reflect = -1.0 #Set to reflect across x axis
-	cube_fits.header['CD1_1'] = -plate_scale*cos(angle) * x_reflect #cd[0,0] #Save rotation and plate scale transformation matrix into fits file header
-	cube_fits.header['CD1_2'] = -plate_scale*sin(angle) #cd[1,0]
-	cube_fits.header['CD2_1'] = -plate_scale*sin(angle) * x_reflect #cd[0,1]
-	cube_fits.header['cd2_2'] = plate_scale*cos(angle) #cd[1,1]
+	cube_fits.header['CD1_1'] = -fits_plate_scale*cos(angle) * x_reflect #cd[0,0] #Save rotation and plate scale transformation matrix into fits file header
+	cube_fits.header['CD1_2'] = -fits_plate_scale*sin(angle) #cd[1,0]
+	cube_fits.header['CD2_1'] = -fits_plate_scale*sin(angle) * x_reflect #cd[0,1]
+	cube_fits.header['CD2_2'] = fits_plate_scale*cos(angle) #cd[1,1]
 	cube_fits.writeto(fname, clobber  = True) #Save fits file
 
 
 
 #Class that stores orion bar cube bar data
-class bar():
+class data():
 	def __init__(self): #Initialize class and create datacube in memory
 		master_cube = zeros(cube_size)  #Initialize array to store flux in a datacube
 		master_cube_var = zeros(cube_size) #Initialize array to store
@@ -70,20 +76,17 @@ class bar():
 		xshift_pix = round(xshift/plate_scale).astype(int) #Find starting x position in pixels for each pionting
 		yshift_pix = round(yshift/plate_scale).astype(int) #Find starting y position in pixels for each pionting
 		for i in xrange(n_pointings): #Loop through each pointing along the scan of the Orion Bar
-			spec1d, spec2d = getspec(date, waveno, framenos[i], stdno, B=B, V=V, y_scale=0.85, wave_smooth=1.0) #Create 1D and 2D spectra objects for all orders combining both H and K bands (easy eh?)
-			#spec1d.subtract_continuum() #Subtract continuum from 1D spectrum, comment out to not subtract continuum
+			spec1d, spec2d = getspec(date, waveno, framenos[i], stdno, B=B, V=V, y_scale=a0v_HI_y_scale, wave_smooth=a0v_HI_gauss_smooth) #Create 1D and 2D spectra objects for all orders combining both H and K bands (easy eh?)
 			spec2d.subtract_continuum() #Subtract continuum from 2D spectrum, comment out to not subtract continuum
 			spec1d.combine_orders() #Combine all orders in 1D spectrum into one very long spectrum
 			spec2d.combine_orders() #Combine all orders in 2D spectrum into one very long spectrum
-			#spec1d.plot() #Plot 1D spectrum
-			#spec2d.plot(spectral_lines, pause = True, close = True)  #Plot 2D spectrum in DS9
 			pv = position_velocity(spec1d.combospec, spec2d.combospec, spectral_lines, shift_lines=v_shift_file) #Extract and create a datacube in position-velocity space of all lines in line list(s) found in spectrum
 			nans = ~isfinite(pv.pv) #Zero out nans before stacking
 			pv.pv[nans] = 0.
 			pv.var2d[nans] = 0.
-			if i==2: #If at the 0,0 position, scale flux down to match other pointings in map (we spend 3x as long here as the other pointings)
-				pv.pv = pv.pv / 3.0
-				pv.var2d = pv.var2d / 9.0
+			if exposure[i] != 1.0: #If exposure times vary
+				pv.pv = pv.pv / exposure[i]  #Scale flux
+				pv.var2d = pv.var2d / (exposure[i]**2) #Scale variance (to propogate uncertainity)
 			flux = repeat(pv.pv[:,:,:,newaxis],4,3) #Set up slice of RA, Dec. grid to put flux into
 			var = repeat(pv.var2d[:,:,:,newaxis],4,3)  #Set up slice of RA, Dec. grid to put variance into
 			x_start = xshift_pix[i] #Set up beginning and ending pixels to paint 
@@ -94,9 +97,6 @@ class bar():
 			master_cube_var[:,y_start:y_end,:,x_start:x_end] = master_cube_var[:,y_start:y_end,:,x_start:x_end] + var  #Load slit into master variance cube
 			used_pixels = flux != 0.0 #Find pixels where we just placed data on the cube grid
 			master_cube_overlap[:,y_start:y_end,:,x_start:x_end][used_pixels] = master_cube_overlap[:,y_start:y_end,:,x_start:x_end][used_pixels] + 1.0 #Increment the counter for how many pointings these pixels used
-			#for j in xrange(x_expand):
-			#	master_cube[y_,:,:,x_expand*i+j] = pv.pv  
-			#	master_cube_var[:,:,:,x_expand*i+j] = pv.var2d
 		master_cube = master_cube / master_cube_overlap #Scale overlapping pixels down by the number of pointings overlapping a given pixel
 		master_cube_var = master_cube_var / master_cube_overlap**2  #Also for the variance: scale overlapping pixels down by the number of pointings overlapping a given pixel
 		empty_pixels = master_cube == 0.0 #Blank out empty pixels with nans
@@ -183,7 +183,7 @@ class bar():
 				cube[i,:,j,:][nans_found] = filled_slice[nans_found] #Fill in the gaps
 				var[i,:,j,:][nans_found]  = filled_slice_var[nans_found] #Fill in the gaps
 		self.cube = cube #Update datacube
-		sefl.var = var #Update datacube variance
+		self.var = var #Update datacube variance
 
 
 
